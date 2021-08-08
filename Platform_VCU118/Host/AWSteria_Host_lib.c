db4471c812c32e86a31c1fd21351726620084aaf
Rishiyur Nikhil <nikhil@acm.org>
2021-08-08 17:32:02 -0400

    Platform_VCU118/Host lib is ready; TestApp/Host main.c builds and links

--- Platform_VCU118/Host/AWSteria_Host_lib.c
+++ Platform_VCU118/Host/AWSteria_Host_lib.c
@@ -1,6 +1,6 @@
 // Copyright (c) 2020-2021 Bluespec, Inc.  All Rights Reserved
 
-// This library implements the BluPont host-side API routines
+// This library implements the AWSteria host-side API routines
 // Bluesim and Verilator sim.
 
 // ================================================================
@@ -13,184 +13,249 @@
 #include <string.h>
 #include <unistd.h>
 #include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
-// ----------------
-// AWS FPGA SDK includes
-
-#include "fpga_pci.h"
-
-// This defines:
-//   int fpga_pci_attach(int slot_id, int pf_id, int bar_id, uint32_t flags, pci_bar_handle_t *handle);
-//   int fpga_pci_poke(pci_bar_handle_t handle, uint64_t offset, uint32_t value);
-//   int fpga_pci_peek(pci_bar_handle_t handle, uint64_t offset, uint32_t *value);
-//   int fpga_pci_detach(pci_bar_handle_t handle);
-
-#include "fpga_mgmt.h"
-#include "fpga_dma.h"
-#include "utils/lcd.h"
-
-#include <sys/syscall.h>
-
-/*
-static inline ssize_t getentropy (void *buf, size_t buflen) {
-    unsigned int flags = 0;
-    return syscall (SYS_getrandom, buf, buflen, flags);
-}
-*/
+#include <sys/types.h>
+#include <sys/mman.h>
 
 // ----------------
 // Project includes
 
-#include "BluPont_Host_Side_API.h"
+#include "AWSteria_Host_lib.h"
 
 // ================================================================
 
+static char AXI4_r_devname[] = "/dev/xdma0_c2h_0";
+static char AXI4_w_devname[] = "/dev/xdma0_h2c_0";
+static char AXI4L_devname[]  = "/dev/xdma0_user";
+
+#define RW_MAX_SIZE  0x7ffff000
+
+#define MAP_SIZE     (32*1024UL)
+
 typedef struct {
-    int  pci_slot_id;
-    int  pci_pf_id;
-    int  pci_bar_id;
-    pci_bar_handle_t  pci_bar_handle;
+    // For AXI4
+    int  pci_AXI4_w_fd;
+    int  pci_AXI4_r_fd;
 
-    int  pci_read_fd;     // For DMA over AXI4
-    int  pci_write_fd;    // For DMA over AXI4
-} BluPont_Host_State;
-
-static
-BluPont_Host_State  blupont_host_state = { 0, 0, 0, PCI_BAR_HANDLE_INIT, -1, -1 };
+    // For AXI4L
+    int  pci_AXI4L_fd;
+    void *map_base;
+} AWSteria_Host_State;
 
 // ================================================================
 // Initialization
 
-void *BluPont_Host_Side_init (void)
+void *AWSteria_Host_init (void)
 {
-    int rc;
+    fprintf (stdout, "%s: AWSteria_Host_init\n", __FUNCTION__);
 
-    // ----------------
-    // Initialize FPGA management library
-    fprintf (stdout, "%s: calling fpga_mgmt_init()\n", __FUNCTION__);
-    rc = fpga_mgmt_init ();    // Note: calls fpga_pci_init ();
-    if (rc != 0) {
-	fprintf (stdout, "%s: fpga_mgmt_init() FAILED: rc = %0d\n",
-		 __FUNCTION__, rc);
-	fprintf (stdout, "%s\n", fpga_mgmt_strerror_long (rc));
+    AWSteria_Host_State *p_state = (AWSteria_Host_State *) malloc (sizeof (AWSteria_Host_State));
+    if (p_state == NULL) {
+	perror ("    malloc AWSteria_Host_State");
 	return NULL;
     }
-    fprintf (stdout, "%s: fpga_mgmt_init() done\n", __FUNCTION__);
-    fprintf (stdout, "    pci_slot_id = %0d\n", blupont_host_state.pci_slot_id);
 
     // ----------------
-    // Open file descriptor for DMA read over AXI4
-    fprintf (stdout, "%s: calling fpga_dma_open_queue(... read)\n", __FUNCTION__);
-    blupont_host_state.pci_read_fd = fpga_dma_open_queue (FPGA_DMA_XDMA,
-							  blupont_host_state.pci_slot_id,
-							  0,        // channel
-							  true);    // is_read
-    if (blupont_host_state.pci_read_fd < 0) {
-	fprintf (stdout, "ERROR: %s: unable to open read-dma queue\n",
-		 __FUNCTION__);
+    fprintf (stdout, "    Opening PCIe/AXI4 write device: %s\n", AXI4_w_devname);
+    p_state->pci_AXI4_w_fd = open (AXI4_w_devname, O_RDWR);
+    if (p_state->pci_AXI4_w_fd < 0) {
+	perror("    open device");
 	return NULL;
     }
-    fprintf (stdout, "%s: opened PCI read-dma queue; pci_read_fd = %0d\n",
-	     __FUNCTION__, blupont_host_state.pci_read_fd);
+    fprintf (stdout, "        pci_AXI4_w_fd = %0d\n", p_state->pci_AXI4_w_fd);
 
     // ----------------
-    // Open file descriptor for DMA write over AXI4
-    fprintf (stdout, "%s: calling fpga_dma_open_queue(... write)\n", __FUNCTION__);
-    blupont_host_state.pci_write_fd = fpga_dma_open_queue(FPGA_DMA_XDMA,
-							  blupont_host_state.pci_slot_id,
-							  0,         // channel
-							  false);    // is_read
-    if (blupont_host_state.pci_write_fd < 0) {
-	fprintf (stdout, "ERROR: %s: unable to open write-dma queue\n",
-		 __FUNCTION__);
+    fprintf (stdout, "    Opening PCIe/AXI4 read device: %s\n", AXI4_r_devname);
+    p_state->pci_AXI4_r_fd = open (AXI4_r_devname, O_RDWR);
+    if (p_state->pci_AXI4_r_fd < 0) {
+	perror("open device");
 	return NULL;
     }
-    fprintf (stdout, "%s: opened PCI write-dma queue; pci_write_fd = %0d\n",
-	     __FUNCTION__, blupont_host_state.pci_write_fd);
+    fprintf (stdout, "        pci_AXI4_r_fd = %0d\n", p_state->pci_AXI4_r_fd);
 
     // ----------------
-    // Attach to PCI for peek/poke calls
-
-    int fpga_pci_attach_flags = 0;
-
-    fprintf (stdout, "%s: calling fpga_pci_attach(): pci_pf_id %0d, pci_bar_id %0d, pci_bar_handle %0x\n",
-	     __FUNCTION__,
-	     blupont_host_state.pci_pf_id,
-	     blupont_host_state.pci_bar_id,
-	     blupont_host_state.pci_bar_handle);
-
-    rc = fpga_pci_attach (blupont_host_state.pci_slot_id,
-			  blupont_host_state.pci_pf_id,
-			  blupont_host_state.pci_bar_id,
-			  fpga_pci_attach_flags,
-			  & blupont_host_state.pci_bar_handle);
-    if (rc != 0) {
-	fprintf (stdout, "    FAILED: rc = %0d\n", rc);
-	fprintf (stdout, "%s\n", fpga_mgmt_strerror_long (rc));
+    fprintf (stdout, "    Opening PCIe/AXI4-Lite read/write device: %s\n", AXI4L_devname);
+    p_state->pci_AXI4L_fd = open (AXI4L_devname, O_RDWR | O_SYNC);
+    if (p_state->pci_AXI4L_fd < 0) {
+	perror("open device");
 	return NULL;
     }
-    fprintf (stdout, "     => pci_bar_handle %0d\n", blupont_host_state.pci_bar_handle);
-    return & blupont_host_state;
+    fprintf (stdout, "        pci_AXI4L_fd = %0d\n", p_state->pci_AXI4L_fd);
+
+    // Memory-map the device
+    fprintf (stdout, "    mmap'ing the AXI4-Lite device\n");
+    p_state->map_base = mmap (0, MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED,
+					 p_state->pci_AXI4L_fd, 0);
+    if (p_state->map_base == (void *)-1) {
+	perror("mmap device");
+	return NULL;
+    }
+    fprintf (stdout, "        Memory map address base: %p\n", p_state->map_base);
+
+    // ----------------
+
+    return p_state;
 }
 
 // ================================================================
 // These are used to communicate with the host-facing AXI4 port on the HW.
 // Return 0 if transfer was ok, non-zero if transfer had error.
 
-int BluPont_AXI4_read (void *opaque,
-		       uint8_t *buffer, const size_t size, const uint64_t address)
+int AWSteria_AXI4_write (void *opaque,
+			 uint8_t *buffer, const size_t size, const uint64_t address)
 {
-    BluPont_Host_State *blupont_host_state = opaque;
+    AWSteria_Host_State *p_state = opaque;
 
-    int rc = fpga_dma_burst_read (blupont_host_state->pci_read_fd, buffer, size, address);
-    return rc;
+    for (uint64_t n_sent = 0; n_sent < size; ) {
+	off_t  rc;
+
+	// Set address
+	rc = lseek (p_state->pci_AXI4_r_fd, address + n_sent, SEEK_SET);
+	if (rc != (address + n_sent)) {
+	    perror("seek device");
+	    fprintf (stdout, "ERROR: %s (size 0x%lx address 0x%0lx)\n",
+		     __FUNCTION__, size, address);
+	    fprintf (stdout, "    seek => rc 0x%lx != address + n_sent 0x%0lx\n",
+		     rc, n_sent);
+	    return -1;
+	}
+
+	// Write data from buffer
+	size_t count = size - n_sent;
+	if (count > RW_MAX_SIZE) count = RW_MAX_SIZE;
+
+	rc = write (p_state->pci_AXI4_w_fd, & (buffer [n_sent]), count);
+	if (rc < 0) {
+	    perror("write device");
+	    fprintf (stdout, "ERROR: %s (size 0x%lx address 0x%0lx)\n",
+		     __FUNCTION__, size, address);
+	    fprintf (stdout, "    write => rc %0ld; n_sent 0x%lx, count 0x%lx\n",
+		     rc, n_sent, count);
+	    return -1;
+	}
+	if (rc != count) {
+	    fprintf (stdout, "NOTE: %s (size 0x%lx address 0x%0lx)\n",
+		     __FUNCTION__, size, address);
+	    fprintf (stdout, "    write => rc %0lx; n_sent 0x%lx, count 0x%lx\n",
+		     rc, n_sent, count);
+	}
+	count += rc;
+    }
+    return 0;
 }
 
-int BluPont_AXI4_write (void *opaque,
+int AWSteria_AXI4_read (void *opaque,
 			uint8_t *buffer, const size_t size, const uint64_t address)
 {
-    BluPont_Host_State *blupont_host_state = opaque;
+    AWSteria_Host_State *p_state = opaque;
 
-    int rc = fpga_dma_burst_write (blupont_host_state->pci_write_fd, buffer, size, address);
-    return rc;
+    for (uint64_t n_sent = 0; n_sent < size; ) {
+	off_t  rc;
+
+	// Set address
+	rc = lseek (p_state->pci_AXI4_r_fd, address + n_sent, SEEK_SET);
+	if (rc != (address + n_sent)) {
+	    perror("seek device");
+	    fprintf (stdout, "ERROR: %s (size 0x%lx address 0x%0lx)\n",
+		     __FUNCTION__, size, address);
+	    fprintf (stdout, "    seek => rc 0x%lx != address + n_sent 0x%0lx\n",
+		     rc, n_sent);
+	    return -1;
+	}
+
+	// Read data into buffer
+	size_t count = size - n_sent;
+	if (count > RW_MAX_SIZE) count = RW_MAX_SIZE;
+
+	rc = read (p_state->pci_AXI4_r_fd, & (buffer [n_sent]), count);
+	if (rc < 0) {
+	    perror("read device");
+	    fprintf (stdout, "ERROR: %s (size 0x%lx address 0x%0lx)\n",
+		     __FUNCTION__, size, address);
+	    fprintf (stdout, "    read => rc %0ld; n_sent 0x%lx, count 0x%lx\n",
+		     rc, n_sent, count);
+	    return -1;
+	}
+	if (rc != count) {
+	    fprintf (stdout, "NOTE: %s (size 0x%lx address 0x%0lx)\n",
+		     __FUNCTION__, size, address);
+	    fprintf (stdout, "    read => rc %0lx; n_sent 0x%lx, count 0x%lx\n",
+		     rc, n_sent, count);
+	}
+	n_sent += rc;
+    }
+    return 0;
 }
 
 // ================================================================
 // These are used to communicate with the host-facing AXI4-Lite port on the HW.
 // Return 0 if transfer was ok, non-zero if transfer had error.
 
-int BluPont_AXI4L_read (void *opaque, uint64_t addr, uint32_t *p_data)
+int AWSteria_AXI4L_write (void *opaque, uint64_t addr, uint32_t data)
 {
-    BluPont_Host_State *blupont_host_state = opaque;
+    AWSteria_Host_State *p_state = opaque;
 
-    int rc = fpga_pci_peek (blupont_host_state->pci_bar_handle, addr, p_data);
-    return rc;
+    uint32_t *p = (uint32_t *) p_state->map_base + addr;
+
+    *p = data;
+    return 0;
 }
 
-int BluPont_AXI4L_write (void *opaque, uint64_t addr, uint32_t data)
+int AWSteria_AXI4L_read (void *opaque, uint64_t addr, uint32_t *p_data)
 {
-    BluPont_Host_State *blupont_host_state = opaque;
+    assert (p_data != NULL);
 
-    int rc = fpga_pci_poke (blupont_host_state->pci_bar_handle, addr, data);
-    return rc;
+    AWSteria_Host_State *p_state = opaque;
+
+    uint32_t *p = (uint32_t *) p_state->map_base + addr;
+
+    *p_data = *p;
+    return 0;
 }
-
-// Note: in AWS these invoke 'fpga_pci_{peek,poke}()', respectively.
-//     blupont_host_state contains a pci_bar_handle_t handle for the two calls.
 
 // ================================================================
 // Host_shutdown takes pointer to state returned by Host_init
 // Return 0 if ok, non-zero if error
 
-int BluPont_Host_Side_shutdown (void *opaque)
+int AWSteria_Host_shutdown (void *opaque)
 {
-    BluPont_Host_State *blupont_host_state = opaque;
+    int rc;
 
-    int rc = fpga_pci_detach (blupont_host_state->pci_bar_handle);
-    if (rc != 0) {
-	fprintf (stdout, "main: fpga_pci_detach() FAILED: rc = %0d\n", rc);
-	return 1;
+    AWSteria_Host_State *p_state = opaque;
+
+    fprintf (stdout, "%s: AWSteria_Host_shutdown\n", __FUNCTION__);
+
+    fprintf (stdout, "    munmap'ing the AXI4-Lite device\n");
+    rc = munmap (p_state->map_base, MAP_SIZE);
+    if (rc < 0) {
+	perror ("munmap device");
+	return rc;
     }
+
+    fprintf (stdout, "    Closing PCIe/AXI4-Lite read/write device: %s\n", AXI4L_devname);
+    rc = close (p_state->pci_AXI4L_fd);
+    if (rc < 0) {
+	perror ("close device");
+	return rc;
+    }
+
+    fprintf (stdout, "    Closing PCIe/AXI4 write device: %s\n", AXI4_w_devname);
+    rc = close (p_state->pci_AXI4_w_fd);
+    if (rc < 0) {
+	perror("close device");
+	return rc;
+    }
+
+    fprintf (stdout, "    Closing PCIe/AXI4 read device: %s\n", AXI4_r_devname);
+    rc = close (p_state->pci_AXI4_r_fd);
+    if (rc < 0) {
+	perror("close device");
+	return rc;
+    }
+
     return 0;
 }
 
